0b200d58d079a13d3c67792aa27b718e3dd492fc
tolbou <ttorubou@gmail.com>
2024-06-11 23:16:36 +0900

    本番用リダイレクトURL

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
-# frozen_string_literal: true
-
+# config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
   {
-    scope: 'email, profile'
+    scope: 'email, profile',
+    redirect_uri: 'https://ishikawatrip.onrender.com/auth/google_oauth2/callback'
   }
 end
 OmniAuth.config.allowed_request_methods = %i[post]
