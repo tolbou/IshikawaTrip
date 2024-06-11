daa28ce3025d7b9ba72c0cab1ba5e47e3a5f8129
tolbou <ttorubou@gmail.com>
2024-06-11 23:23:54 +0900

    コメントアウト追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
   {
     scope: 'email, profile',
+    # 本番環境用
     redirect_uri: 'https://ishikawatrip.onrender.com/auth/google_oauth2/callback'
   }
 end
